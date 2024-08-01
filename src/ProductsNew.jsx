c3c9fa3111780183c1463083129b831c3259a92a
Michael Tuhaimer <michaeltuhaimer@Michaels-MacBook-Air.local>
2024-08-01 10:47:32 -0500

    Add new/create actions

---
+++ src/ProductsNew.jsx
@@ -0,0 +1,25 @@
+export function ProductsNew({ onCreate }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    onCreate(params, () => event.target.reset());
+  };
+
+  return (
+    <div>
+      <h1>New Product</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Name: <input name="name" type="text" />
+        </div>
+        <div>
+          Price: <input name="price" type="text" />
+        </div>
+        <div>
+          Description: <input name="description" type="text" />
+        </div>
+        <button type="submit">Create product</button>
+      </form>
+    </div>
+  );
+}
